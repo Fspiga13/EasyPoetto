96f3b02ab2b2bdcd906d5c89c3a4bfb5bd1a7308
Administrator <Administrator@MININT-VL0JBHG>
2019-07-30 10:32:36 +0200

    mod reservation e create reservation servlet

---
+++ src/com/easypoetto/controller/ReservationServlet.java
@@ -0,0 +1,71 @@
+package com.easypoetto.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.easypoetto.model.UserFactory;
+
+/**
+ * Servlet implementation class ReservationServlet
+ */
+@WebServlet("/reservation.html")
+public class ReservationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReservationServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Verificare se loggato
+				HttpSession session = request.getSession(false);
+				
+				if(session == null || session.getAttribute("email") == null || session.getAttribute("password") == null 
+						|| session.getAttribute("role") == null) {	
+					
+					response.sendRedirect("login.html");
+					
+				}else {
+				
+					String error = (String) session.getAttribute("error");
+					session.removeAttribute("error");
+					
+					String success = (String) session.getAttribute("success");
+					session.removeAttribute("success");
+					
+					String email = (String) session.getAttribute("email");
+					String password = (String) session.getAttribute("password");
+					Integer role = (Integer) session.getAttribute("role");
+					
+					if (email!= null && password != null && role != null &&
+							!email.isEmpty() && !password.isEmpty() && role >= 0 && role <= 2 &&
+							UserFactory.getInstance().login(email, password) == role){
+						
+						request.setAttribute("error", error);
+						request.setAttribute("success", success);
+					}
+				}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
